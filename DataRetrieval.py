1b1b6a78bea3b03505c91897eac6f11c2efa3087
knikharg <knikharg@iu.edu>
2020-02-11 12:33:12 -0500

    Changes aligned with model execution

--- DataRetrieval.py
+++ DataRetrieval.py
@@ -1,7 +1,4 @@
 
-"""
-Access NEXRAD radar data via Amazon Web Services and plot with MetPy
-"""
 # Access data 
 import json
 import pika
@@ -35,12 +32,10 @@ channelPublish.queue_declare(queue='model_execution')
 
 def consumer_callback(ch, method, properties, body):
     data =json.loads(body)
-    print(data)
+    print("Consumed from data_retrieval")
     date = data['date']
     month, day, year = date.split('/')
-    print(type(month),day,year)
     radars = conn.get_avail_radars(year,month,day)
-    print(type)
     radar_station = data['radar_station']
     availscans = conn.get_avail_scans(year,month,day, radar_station)
     
@@ -48,7 +43,6 @@ def consumer_callback(ch, method, properties, body):
     start = central_timezone.localize(datetime(int(year),int(month),int(day),17,0))
     end = central_timezone.localize (datetime(int(year),int(month),int(day),19,0))
     scans = conn.get_avail_scans_in_range(start, end, radar_station)
-    print("There are {} scans available between {} and {}\n".format(len(scans), start, end))
     results = conn.download(scans[0], templocation)
     print(results.success)
     file = []
@@ -57,7 +51,7 @@ def consumer_callback(ch, method, properties, body):
         print("{} volume scan time {}".format(scan.radar_id,scan.scan_time))
         file.append(scan.filepath)
     Obj = {"file": file}
-    print(Obj)
+    print("Publishing to model_execution")
     channelPublish.basic_publish(exchange='', routing_key='model_execution', body=json.dumps(Obj))
     
 
