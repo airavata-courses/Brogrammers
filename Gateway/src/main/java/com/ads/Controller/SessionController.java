8f57a4a78baa4d07b6cc2beedc48db57b304b013
Ishita Kumar <kumar.ishita@Gmail.com>
2020-02-11 19:23:04 -0500

    merge conflicts fixed

--- Gateway/src/main/java/com/ads/Controller/SessionController.java
+++ Gateway/src/main/java/com/ads/Controller/SessionController.java
@@ -1,6 +1,7 @@
 package com.ads.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import com.ads.pojo.UserSession;
 
 @RestController
 @RequestMapping("/rest")
+@CrossOrigin(value = "http://localhost:3000")
 public class SessionController {
 
 	@Autowired
