a2cbf2c60210cf8e8c7fd04f0750d5211fcee7a0
knikharg <knikharg@iu.edu>
2020-02-13 12:20:13 -0500

    modifications to data retrieval

--- DataRetrieval.py
+++ DataRetrieval.py
@@ -18,25 +18,20 @@ connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-channel.queue_declare(queue='data_retrieval')
+channel.queue_declare(queue='data-retrieval-reflectivity')
 
 # producer queue 
-# Publish created url
-connectionPublish = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
-channelPublish = connectionPublish.channel()
-
-channelPublish.queue_declare(queue='model_execution')
+channel.queue_declare(queue='model-execution')
 
 
 
 def consumer_callback(ch, method, properties, body):
     data =json.loads(body)
-    print("Consumed from data_retrieval")
+    print("Consumed from data_retrieval", data)
     date = data['date']
-    month, day, year = date.split('/')
+    year, month, day = date.split('-')
     radars = conn.get_avail_radars(year,month,day)
-    radar_station = data['radar_station']
+    radar_station = data['radar']
     availscans = conn.get_avail_scans(year,month,day, radar_station)
     
     central_timezone = pytz.timezone('US/Central')
@@ -52,14 +47,14 @@ def consumer_callback(ch, method, properties, body):
         file.append(scan.filepath)
     Obj = {"file": file}
     print("Publishing to model_execution")
-    channelPublish.basic_publish(exchange='', routing_key='model_execution', body=json.dumps(Obj))
+    channel.basic_publish(exchange='', routing_key='model-execution', body=json.dumps(Obj))
     
 
 
 channel.basic_consume(
-    queue='data_retrieval', on_message_callback=consumer_callback, auto_ack=True)
+    queue='data-retrieval-reflectivity', on_message_callback=consumer_callback, auto_ack=True)
 
 channel.start_consuming()
 
-channelPublish.close()
+
 
