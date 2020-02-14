ea59acae98d380352344c1312881e6adabceb6b7
knikharg <knikharg@iu.edu>
2020-02-13 23:55:59 -0500

    last

--- Front_End_React/src/components/Navbar.js
+++ Front_End_React/src/components/Navbar.js
@@ -9,14 +9,14 @@ export default class Navbar extends Component {
         return (
             <div>
            <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
-  <a class="navbar-brand"><Link to="/dashboard"></Link> Brogrammers</a>
+  <a class="navbar-brand"><Link to="/dashboard"> Brogrammers</Link></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarText">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link"  ><Link to="/"></Link> Logout</a>
+        <a class="nav-link"  ><Link to="/">Logout</Link> </a>
       </li>
       
     </ul>
