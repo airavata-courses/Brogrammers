89289b860dab3d733c1fc33ad75c89ecc122b077
ArjunBhavsar <bhavsararjun@gmail.com>
2020-02-25 18:08:50 -0500

    Issue for maven build Resolved

--- UserManagement/src/test/java/com/brogrammer/accessdatauser/UserManagementApplicationTests.java
+++ UserManagement/src/test/java/com/brogrammer/accessdatauser/UserManagementApplicationTests.java
@@ -3,7 +3,7 @@ package com.brogrammer.accessdatauser;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = UserManagementApplicationTests.class)
 class UserManagementApplicationTests {
 
 	@Test
