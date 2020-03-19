74cab4e9785f23d59110f067244ef25bfa81be97
Arjun Dattatray Bhavsar <bhavsararjun@gmail.com>
2020-03-19 01:25:27 -0400

    Update ActivityController.java
    Handled CoRS issue

--- src/main/java/com/gateway/controller/ActivityController.java
+++ src/main/java/com/gateway/controller/ActivityController.java
@@ -13,7 +13,7 @@ import com.gateway.service.SessionService;
 
 @RestController()
 @RequestMapping("/rest")
-@CrossOrigin(origins = {"http://frontend:3000","http://frontend:3001"})
+@CrossOrigin
 public class ActivityController {
 	
 	@Autowired
