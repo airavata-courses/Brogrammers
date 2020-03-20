c33254ce6b5f2880ab3e7efbc24b898f842d56ad
knikharg <knikharg@iu.edu>
2020-03-19 21:22:46 -0400

    Added logging

--- ModelExecution.py
+++ ModelExecution.py
@@ -5,17 +5,18 @@ import pytz
 templocation = tempfile.mkdtemp()
 import pika
 import json
-
+import logging
 import time
 
 time.sleep( 50 )
+logging.basicConfig()
 
 # establishing connection to RabbitMQ server
 credentials = pika.PlainCredentials(username='guest', password='guest')
 connection = pika.BlockingConnection(pika.ConnectionParameters(
             host = 'rabbitmq-service' , port=5672, credentials=credentials))
  
-print ("Connection Established")
+logging.info("Connection Established")
 
 channel = connection.channel()
  # Publisher 
@@ -23,15 +24,11 @@ channel.queue_declare(queue='post-analysis-reflectivity')
 data_send = []
 def callback(ch, method, properties, body):
     data = json.loads(body) 
-    print(data)
-    print("Consumed from model_execution")
-    print("Publishing to post_analysis")
-    print("Data sent: ",data)
+    logging.debug("Data sent: ",data)
     channel.basic_publish(exchange='', routing_key='post-analysis-reflectivity', body=json.dumps(data))
         
 
-channel.basic_consume(
-queue='model-execution', on_message_callback=callback, auto_ack=True)
+channel.basic_consume(queue='model-execution', on_message_callback=callback, auto_ack=True)
 
 channel.start_consuming()
 
