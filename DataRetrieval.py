d46987a67579eff6687f00db8d499b29fb1c75a3
knikharg <knikharg@iu.edu>
2020-03-20 10:49:14 -0400

    Testing

--- DataRetrieval.py
+++ DataRetrieval.py
@@ -21,7 +21,7 @@ credentials = pika.PlainCredentials(username='guest', password='guest')
 connection = pika.BlockingConnection(pika.ConnectionParameters(
             host = 'rabbitmq-service' , port=5672, credentials=credentials))
 
-print ("Connection Established")
+
 channel = connection.channel()
 
 channel.queue_declare(queue='data-retrieval-reflectivity')
@@ -33,7 +33,7 @@ channel.queue_declare(queue='model-execution')
 
 def consumer_callback(ch, method, properties, body):
     data =json.loads(body)
-    logging.debug("Consumed from data_retrieval", data)
+    logging.info("Connection Established")
     date = data['date']
     year, month, day = date.split('-')
     radars = conn.get_avail_radars(year,month,day)
@@ -45,14 +45,14 @@ def consumer_callback(ch, method, properties, body):
     end = central_timezone.localize (datetime(int(year),int(month),int(day),19,0))
     scans = conn.get_avail_scans_in_range(start, end, radar_station)
     results = conn.download(scans[0], templocation)
-    print(results.success)
+    logging.info("Result scan ",results.success)
     file = []
 
     for scan in results.iter_success():
         print("{} volume scan time {}".format(scan.radar_id,scan.scan_time))
         file.append(scan.filepath)
     Obj = {"file": file}
-    logging.debug("Publishing to model_execution")
+    logging.info("Publishing to model_execution")
     channel.basic_publish(exchange='', routing_key='model-execution', body=json.dumps(Obj))
     
 
