4fa3e4b2ee2d2676c81536d30a4b951c328b33d3
ishita-kumar <kumar.ishita@gmail.com>
2020-04-19 20:48:17 -0400

    changed

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-# base image
+# # base image
 FROM node:12
-#FROM node:10.2.0-alpine
+# #FROM node:10.2.0-alpine
 
 RUN mkdir /code
 WORKDIR /code
@@ -9,16 +9,25 @@ RUN npm install
 
 RUN npm install axios 
 EXPOSE 3000
-CMD ["npm","run","build"]
+CMD ["npm","start"]
 
 # FROM node:alpine as builder
 # WORKDIR /app
 # COPY . ./
 # RUN npm install
-# RUN npm run build
 
-# FROM nginx:alpine
-# COPY nginx.conf /etc/nginx/conf.d/default.conf
-# COPY --from=builder /app/build /usr/share/nginx/html
+# RUN npm install axios 
 # EXPOSE 3000
-# CMD ["nginx", "-g", "daemon off;"]
+# CMD ["npm","start"]
+
+FROM node:alpine as builder
+WORKDIR /app
+COPY . ./
+RUN npm install
+RUN npm run build
+
+FROM nginx:alpine
+COPY nginx.conf /etc/nginx/conf.d/default.conf
+COPY --from=builder /app/build /usr/share/nginx/html
+EXPOSE 3000
+CMD ["nginx", "-g", "daemon off;"]
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,22 +4,30 @@ import { Link } from 'react-router-dom';
 
 
 export default class Navbar extends Component {
+  constructor(props) {
+    super(props)
+  }
 
-    render() {
-        return (
-            <div>
+  logoutHandler = (e) => {
+    this.props.history.push('/login')
+  }
+  render() {
+    return (
+      <div>
 
-           <nav class="navbar   navbar-dark bg-dark">
-  <a class="navbar-brand"  href="#"><Link to="/dashboard" style={{color:"white" , textDecoration:"none"}}>Brogrammers</Link></a>
-  <ul class="navbar-nav ">
-    <li class="nav-item active w-100">
-        <a class="nav-link"><Link to="/" style={{color:"white", textDecoration:"none" }}>Logout</Link> </a>
-      </li>
-    </ul>
-  {/* <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <nav class="navbar   navbar-dark bg-dark">
+          <a class="navbar-brand" href="#"><Link to="/dashboard" style={{ color: "white", textDecoration: "none" }}>Brogrammers</Link></a>
+          <ul class="navbar-nav ">
+            <li class="nav-item active w-100">
+            <a href="#" onClick={e=>this.logoutHandler(e)}>Lokkkgout</a>
+
+              {/* <a class="nav-link"><Link to="/" style={{ color: "white", textDecoration: "none" }}>Logout</Link> </a> */}
+            </li>
+          </ul>
+          {/* <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button> */}
-  {/* <div class="collapse navbar-collapse" id="navbarText">
+          {/* <div class="collapse navbar-collapse" id="navbarText">
     <ul class="navbar-nav mr-auto">
     <li class="nav-item active">
         <a class="nav-link"  ><Link to="/">Logout</Link> </a>
@@ -29,9 +37,9 @@ export default class Navbar extends Component {
    
   </div> */}
 
-  
-</nav>
-</div>
-        )
-    }
+
+        </nav>
+      </div>
+    )
+  }
 }
