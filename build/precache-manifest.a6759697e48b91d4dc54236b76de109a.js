71fe01716fa599f1ef6bf40b6cf2fdfd6b37cab9
ishita-kumar <kumar.ishita@gmail.com>
2020-04-20 20:39:13 -0400

    went back to old commit

---
+++ build/precache-manifest.a6759697e48b91d4dc54236b76de109a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ccb1ed2ab85c349c1421d6d48d2f115",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25890350789b940ceefd",
+    "url": "/static/css/2.7e40b136.chunk.css"
+  },
+  {
+    "revision": "fb08d20017c691bb56b2",
+    "url": "/static/css/main.64a4d606.chunk.css"
+  },
+  {
+    "revision": "25890350789b940ceefd",
+    "url": "/static/js/2.af027957.chunk.js"
+  },
+  {
+    "revision": "6c3984842d5ebc9b6db6fca2969c6a5c",
+    "url": "/static/js/2.af027957.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb08d20017c691bb56b2",
+    "url": "/static/js/main.b5ca4755.chunk.js"
+  },
+  {
+    "revision": "77b8dad9641a813b46f8",
+    "url": "/static/js/runtime-main.ea6d0dd5.js"
+  },
+  {
+    "revision": "f080ad0c28f778e843a1b37e3f9c4cae",
+    "url": "/static/media/bgg.f080ad0c.jpg"
+  },
+  {
+    "revision": "846cd49529b67a8dff05c8e156481fed",
+    "url": "/static/media/giphy.846cd495.gif"
+  }
+]);
