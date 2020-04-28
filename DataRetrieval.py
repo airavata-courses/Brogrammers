f3fe3121a70efe90be8d8c0cad398a87a6adcda9
Arjun Dattatray Bhavsar <bhavsararjun@gmail.com>
2020-04-28 16:16:19 -0400

    Update DataRetrieval.py
    testing Connection of RabbitMQ

--- DataRetrieval.py
+++ DataRetrieval.py
@@ -17,8 +17,7 @@ conn = nexradaws.NexradAwsInterface()
 
 # establishing connection to RabbitMQ server
 credentials = pika.PlainCredentials(username='guest', password='guest')
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-            host = 'rabbitmq' , port=5672, credentials=credentials))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host = 'rabbitmq' , port=5672, credentials=credentials))
 
 #connection = pika.BlockingConnection(pika.ConnectionParameters(
 #            host = 'localhost',credentials=credentials)))
@@ -28,7 +27,7 @@ channel.queue_declare(queue='data-retrieval-reflectivity')
 
 # producer queue 
 logging.warning("Connection established")
-channel.queue_declare(queue='model-execution')
+
 logging.warning("Connection Established Waiting for data")
 def consumer_callback(ch, method, properties, body):
     data =json.loads(body)
@@ -52,12 +51,22 @@ def consumer_callback(ch, method, properties, body):
         print("{} volume scan time {}".format(scan.radar_id,scan.scan_time))
         file.append(scan.filepath)
     Obj = {"file": file}
-    logging.warning("Publishing to model_execution")
+    logging.warning("Publishing to model_execution",Obj)
     print("Data sent",Obj)
-    channel.basic_publish(exchange='', routing_key='model-execution', body=json.dumps(Obj))
-    
-channel.basic_consume(	
-    queue='data-retrieval-reflectivity', on_message_callback=consumer_callback, auto_ack=True)
-channel.start_consuming()
 
+
+    microservice_connection = pika.BlockingConnection(pika.ConnectionParameters(host = 'rabbitmq' , port=5672, credentials=credentials))
+    microservice_channel = microservice_connection.channel()
+    #microservice_channel.exchange_declare(exchange='model-execution', exchange_type='topic')
+    microservice_channel.queue_declare(queue='model-execution1')
+    microservice_channel.basic_publish(exchange='', routing_key='model-execution1', body=json.dumps(Obj))
+    logging.warning("Published to model_execution")
+
+while TRUE:
+            
+     channel.basic_consume(queue='data-retrieval-reflectivity', on_message_callback=consumer_callback, auto_ack=True)
+     channel.start_consuming()
+     logging.warning("Data Recieved from gateway")
+     connection = pika.BlockingConnection( pika.ConnectionParameters(host = 'rabbitmq' , port = 5672 , credentials = credentials ) )
+     channel = connection.channel( )
 
